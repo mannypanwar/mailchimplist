431762f803c516caa3fd21a1d55c8ce376b3d690
Mandeep Singh Panwar <fit.panwar@gmail.com>
2020-05-25 12:47:31 +0530

    final build

--- app.js
+++ app.js
@@ -38,13 +38,23 @@ app.post("/", (req, res) => {
     auth: "manny:91ce4c3616cfb87b37230f4d8e60add7-us19",
   };
   const request = https.request(url, options, (response) => {
+    if (response.statusCode == 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
     response.on("data", (data) => {
       console.log(JSON.parse(data));
     });
   });
 
   request.write(jsonData);
-  request.end;
+  request.end();
+});
+
+app.post("/failure", (req, res) => {
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT || 3000, () => {
